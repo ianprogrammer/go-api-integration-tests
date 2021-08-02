12ab8cf59ed176fde1693549519ee78815863f5e
Ian Oliveira <ianprogrammer@gmail.com>
2021-08-02 00:31:53 -0300

    test: add repository test using testcontainers-go

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/ianprogrammer/go-api-integration-test
 go 1.16
 
 require (
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.4.0 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/testcontainers/testcontainers-go v0.11.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- pkg/product/product_repository_test.go
+++ pkg/product/product_repository_test.go
@@ -1,1 +1,121 @@
 package product_test
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/ianprogrammer/go-api-integration-test/config"
+	"github.com/ianprogrammer/go-api-integration-test/internal/database"
+	"github.com/ianprogrammer/go-api-integration-test/pkg/product"
+	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/require"
+	tc "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"gorm.io/gorm"
+)
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+var (
+	_sut  *product.Repository
+	_conn *gorm.DB
+)
+
+func TestMain(m *testing.M) {
+	log.Println("Starting postgres container...")
+	postgresPort := nat.Port("5432/tcp")
+	postgres, err := tc.GenericContainer(context.Background(),
+		tc.GenericContainerRequest{
+			ContainerRequest: tc.ContainerRequest{
+				Image:        "postgres:12.2-alpine",
+				ExposedPorts: []string{postgresPort.Port()},
+				Env: map[string]string{
+					"POSTGRES_PASSWORD": "pass",
+					"POSTGRES_USER":     "user",
+				},
+				WaitingFor: wait.ForAll(
+					wait.ForLog("database system is ready to accept connections"),
+					wait.ForListeningPort(postgresPort),
+				),
+			},
+			Started: true,
+		})
+	if err != nil {
+		log.Fatal("start:", err)
+	}
+
+	hostPort, err := postgres.MappedPort(context.Background(), postgresPort)
+	if err != nil {
+		log.Fatal("map:", err)
+	}
+
+	port, err := strconv.Atoi(hostPort.Port())
+
+	if err != nil {
+		log.Fatal("convert port to int went wrong", err)
+	}
+
+	databaseConfig := config.DatabaseConfig{
+		Host:         "localhost",
+		UserName:     "user",
+		Password:     "pass",
+		DatabaseName: "postgres",
+		DatabasePort: port,
+	}
+
+	_conn, err = database.NewDatabase(databaseConfig)
+
+	if err != nil {
+		log.Fatal("could not be possible to connect to database")
+	}
+
+	database.MigrateDB(_conn)
+	_sut = &product.Repository{
+		DB: _conn,
+	}
+	os.Exit(m.Run())
+}
+
+func TestRepoImpl(t *testing.T) {
+
+	t.Run("create and get a product", func(t *testing.T) {
+
+		t.Log("should save the product into database and return the product.")
+		{
+			products := []product.Product{
+				{
+					Name:  "Test product 1",
+					Price: 100,
+				},
+				{
+					Name:  "Test product 3",
+					Price: 100,
+				},
+				{
+					Name:  "Test product 2",
+					Price: 100000,
+				},
+			}
+			var countInserted int
+			for i, p := range products {
+				t.Logf("\tTest %d: When passing product  with name of %s and price of %d", i, p.Name, p.Price)
+				{
+					product, err := _sut.Insert(p)
+					require.NoError(t, err)
+					if product.Name == p.Name {
+						t.Logf("\t%s\t  Product %s was inserted.", succeed, p.Name)
+					} else {
+						t.Logf("\t%s\t  Product %s was failed.", failed, p.Name)
+					}
+					countInserted++
+				}
+			}
+			assert.Equal(t, countInserted, len(products))
+		}
+	})
+}
